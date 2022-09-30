47440d33737e354c08afe9ca483922b79808831b
sea-yassan33 <a.yasuda_tr51@me.com>
2022-10-01 08:53:33 +0900

    ツイートを保存機能

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -2,4 +2,17 @@ class TweetsController < ApplicationController
   def index
     @tweets = Tweet.all
   end
+
+  def new
+    @tweet = Tweet.new
+  end
+
+  def create
+    Tweet.create(tweet_params)
+  end
+
+  pribate
+  def tweet_params
+    params.require(:tweet).permit(:name, :image, :text)
+  end
 end
